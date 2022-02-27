553aa26c0bb71c68b6286d17740aa4cf37c8f024
Krishna7pk <98844700+Krishna7pk@users.noreply.github.com>
2022-02-26 23:44:32 -0500

    Update Bidding.tsx

--- screens/Bidding.tsx
+++ screens/Bidding.tsx
@@ -4,66 +4,79 @@ import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, ScrollView, Ima
 //  Dropdown }
 //  from 'react-native-material-dropdown';
 
-const Bidding = (navigation) =>{
+const Bidding = (navigation) => {
 
-    return(
+    return (
         <ScrollView>
-            <SafeAreaView style ={styles.navHeader}>
+            {/* <SafeAreaView style ={styles.navHeader}>
                 <Button title="<" onPress={() => navigation.goBack()} />
-            </SafeAreaView>
-            <View style = {styles.wrapper}>
-                <SafeAreaView style={styles.menu}>
-                        <Image
-                            source={require('../assets/shovl_new_logo.png')}
-                            resizeMode="center"
-                            style={styles.logoimage}
+            </SafeAreaView> */}
+            <View style={styles.wrapper}>
+                <View style={styles.menu}>
+                    <Image
+                        source={require('../assets/snowflakes.png')}
+                        resizeMode="center"
+                        style={styles.logoimage}
+                    />
+                    <Text style={styles.tag}>Bidding</Text>
+                    <Text>Home</Text></View>
+                <View style={styles.formElements}>
+                    <Image
+                        source={require('../assets/snowAdvertise.jpeg')}
+                        resizeMode="center"
+                        style={styles.image}
+                    />
+
+                    <View style={styles.labelName}>
+                        <Text style={{ width: "20%" }}>Location:</Text>
+                        <TextInput
+                            style={styles.textInput}
+                            value="22, Maple view, Barrie, Ontario" multiline={true} numberOfLines={3} editable={false} selectTextOnFocus={false}
                         />
-                        <Text style={styles.tag}>Bidding</Text>
-                        <Text>Home</Text>
-                </SafeAreaView>
-            </View>
-            <View /*style = {styles.bodyimg}*/>
-                     <Image source={require('../assets/locations/2.jpeg')}
-                     resizeMode="stretch"
-                     style = {styles.bodyimg}                     />
-                
-                <Text style = {styles.bidpricetext}> Bid Your Price</Text>
-                <TextInput 
-                        style={styles.textInput}
-                        placeholder="$__"
-                    />
-                <Text style = {styles.reqtime}> Required Time </Text>
-                <TextInput 
-                        style={styles.textInput}
-                        placeholder="$__Hour"
-                    />
-                    <TouchableOpacity 
-                 style={styles.touchbutonStyle1} 
-                 onPress={()=>{}} >
+                    </View>
+                    <View style={styles.labelName}>
+                        <Text style={{ width: "20%" }}>Bid Your Price:</Text>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder="$__"
+                        />
+                    </View>
+                    <View style={styles.labelName}>
+                        <Text style={{ width: "20%" }}>Required Time:</Text>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder="$__Hour"
+                        />
+                    </View>
+                    <View style={styles.flexrow}>
+                        <TouchableOpacity onPress={() => { }} >
+                            <Text style={styles.buttonStyle}>Submit</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { }} >
+                            <Text style={styles.cancelButton}>Cancel</Text>
+                        </TouchableOpacity>
 
-                    <Text style={styles.buttonTextStyle}>Procced</Text>
-                
-                 </TouchableOpacity>
-                 <TouchableOpacity 
-                 style={styles.touchbutonStyle2} 
-                 onPress={()=>{}} >
-
-                    <Text style={styles.buttonTextStyle}>Cancel</Text>
-                
-                 </TouchableOpacity>
+                    </View>
 
                 </View>
+            </View>
 
         </ScrollView>
     )
 
-} 
+}
 
 const styles = StyleSheet.create({
-    wrapper:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
+    wrapper: {
+        flex: 1,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    labelName: {
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center"
     },
     menu: {
         // flex: 1,
@@ -75,76 +88,82 @@ const styles = StyleSheet.create({
         borderColor: "#0f0f0f",
         justifyContent: "space-between"
     },
-    textInput:{
+    logoimage:
+        { width: 30, height: 30 },
+    tag:
+        { fontSize: 20, fontWeight: "500" },
+    image: {
+        width: 400,
+        height: 200,
+        marginTop: 20
+    },
+    textInput: {
         textAlign: 'center',
-        color:'#126fb2',
-        borderWidth:2,
-        borderColor:'#126fb2',
+        color: '#126fb2',
+        borderWidth: 2,
+        borderColor: '#126fb2',
         padding: 8,
         margin: 5,
-        alignSelf:"center",
+        alignSelf: "center",
         marginTop: 15,
-        width: 300,
+        width: "65%",
         borderRadius: 8,
         fontSize: 16
     },
-    logoimage: 
-    {
-        width: 60, 
-        height: 30, 
-    },
-    bodyimg:{
-        width:410,
-        height:330,
-        alignSelf:"center",
-    },
-    bidpricetext:{
-        textAlign:'left',
-        fontSize: 20,
-        marginTop:10,
-        color:'#126fb2',
-        alignSelf:"center"
-    },
-    reqtime:{
-        textAlign:'left',
-        fontSize: 20,
-        marginTop:5,
-        color:'#126fb2',
-        alignSelf:"center"
-    },
-    tag:{
-        fontSize: 20, fontWeight: "500" 
-    },
-    touchbutonStyle1:{
-        alignSelf:"center",
-        backgroundColor:'#126fb2',
+    formElements: {
         alignItems: 'center',
-        borderRadius:8,
-        padding: 10, 
-        marginTop:42,
-        marginBottom: 1,
-        width:250,
+        justifyContent: 'center',
     },
-    touchbutonStyle2:{
-        alignSelf:"center",
-        backgroundColor:'#126fb2',
-        alignItems: 'center',
-        borderRadius:8,
-        padding: 10, 
-        marginTop:20,
-        marginBottom: 1,
-        width:250,
+    bodyimg: {
+        width: 400,
+        height: 200,
+        marginTop: 20,
+        marginBottom: 10
     },
-  buttonTextStyle: {
-        color: "white",
-        alignSelf:"center",
-        fontSize:18,
-        fontWeight: "bold",
-  },
-  navHeader:{
-    marginLeft:"5%",
-    alignItems : 'flex-start'
-  },
+    bidpricetext: {
+        textAlign: 'left',
+        fontSize: 20,
+        marginTop: 10,
+        color: '#126fb2',
+        alignSelf: "center"
+    },
+    reqtime: {
+        textAlign: 'left',
+        fontSize: 20,
+        marginTop: 5,
+        color: '#126fb2',
+        alignSelf: "center"
+    },
+    flexrow: {
+        flexDirection: "row",
+
+    },
+    buttonStyle: {
+        backgroundColor: '#126fb2',
+        borderRadius: 8,
+        padding: 10,
+        color: "#fff",
+        margin: 20,
+        width: 100,
+        textAlign: "center",
+        fontWeight: "500"
+    },
+    cancelButton: {
+        backgroundColor: '#900',
+        borderRadius: 8,
+        padding: 10,
+        color: "#fff",
+        margin: 20,
+        width: 100,
+        textAlign: "center",
+        fontWeight: "500"
+
+    },
+
+    navHeader: {
+        marginLeft: "5%",
+        alignItems: 'flex-start'
+    },
 })
 
 export default Bidding;
